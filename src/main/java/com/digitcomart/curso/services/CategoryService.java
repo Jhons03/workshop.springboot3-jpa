db384b2d2c9d05c695683f45d63aaabb2587c936
Jhons03 <edin2008@yahoo.com.br>
2023-07-24 00:29:53 -0300

    Category

---
+++ src/main/java/com/digitcomart/curso/services/CategoryService.java
@@ -0,0 +1,27 @@
+package com.digitcomart.curso.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.digitcomart.curso.entities.Category;
+import com.digitcomart.curso.repositories.CategoryRepository;
+
+@Service
+public class CategoryService {
+	
+	@Autowired
+	private CategoryRepository repository;
+	
+	public List<Category> findAll() {
+		return repository.findAll();
+	}
+	
+	public Category findById(Long id) {
+		Optional<Category> obj = repository.findById(id);
+		return obj.get();
+	}
+
+}
